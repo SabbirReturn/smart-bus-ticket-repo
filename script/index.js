4e245a6f8076b7f1337ba3caf0fc1ea1feeb94f3
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-03-30 16:20:31 +0600

    buy ticket btn

---
+++ script/index.js
@@ -0,0 +1,4 @@
+document.getElementById('buy_ticket_btn').addEventListener('click', function(){
+    let targetSection = document.getElementById('target');
+    targetSection.scrollIntoView({behavior:"smooth"})
+})
