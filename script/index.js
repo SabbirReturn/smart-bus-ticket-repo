8ab8223d9d35d3ffa476397679017115d066529f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-03-31 18:18:41 +0600

    recheck

--- script/index.js
+++ script/index.js
@@ -1,4 +1,9 @@
+// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
+//     let targetSection = document.getElementById('target');
+//     targetSection.scrollIntoView({behavior:"smooth"})
+// })
 document.getElementById('buy_ticket_btn').addEventListener('click', function(){
     let targetSection = document.getElementById('target');
     targetSection.scrollIntoView({behavior:"smooth"})
 })
+
