ef31801866bbd82a5858f55fe5c3e8d03e9510a3
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-04-07 23:22:36 +0600

    again

--- script/index.js
+++ script/index.js
@@ -24,6 +24,10 @@
 //     let targetSection = document.getElementById('target');
 //     targetSection.scrollIntoView({behavior:"smooth"})
 // })
+// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
+//     let targetSection = document.getElementById('target');
+//     targetSection.scrollIntoView({behavior:"smooth"})
+// })
 document.getElementById('buy_ticket_btn').addEventListener('click', function(){
     let targetSection = document.getElementById('target');
     targetSection.scrollIntoView({behavior:"smooth"})
