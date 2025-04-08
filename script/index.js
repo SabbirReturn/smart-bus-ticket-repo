c5d21880d511e7aada970451b5e417977bfeebef
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-04-08 23:15:27 +0600

    again

--- script/index.js
+++ script/index.js
@@ -1,29 +1,4 @@
-// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
-//     let targetSection = document.getElementById('target');
-//     targetSection.scrollIntoView({behavior:"smooth"})
-// })
-// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
-//     let targetSection = document.getElementById('target');
-//     targetSection.scrollIntoView({behavior:"smooth"})
-// })
-// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
-//     let targetSection = document.getElementById('target');
-//     targetSection.scrollIntoView({behavior:"smooth"})
-// })
 
-// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
-//     let targetSection = document.getElementById('target');
-//     targetSection.scrollIntoView({behavior:"smooth"})
-// })
-
-// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
-//     let targetSection = document.getElementById('target');
-//     targetSection.scrollIntoView({behavior:"smooth"})
-// })
-// document.getElementById('buy_ticket_btn').addEventListener('click', function(){
-//     let targetSection = document.getElementById('target');
-//     targetSection.scrollIntoView({behavior:"smooth"})
-// })
 // document.getElementById('buy_ticket_btn').addEventListener('click', function(){
 //     let targetSection = document.getElementById('target');
 //     targetSection.scrollIntoView({behavior:"smooth"})
